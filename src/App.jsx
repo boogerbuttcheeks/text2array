67499ac1b20b3514f5931148a62042dda5a6b24e
Trevor Lee <tlee297@my.bcit.ca>
2022-12-29 17:49:32 -0800

    add quick fill function

--- src/App.jsx
+++ src/App.jsx
@@ -79,6 +79,10 @@ function App() {
     setInput(e.target.value);
   }
 
+  function quickFill(e) {
+    setInput(e)
+  }
+
   return (
     <div className='app'>
       <h1>Text 2 Array</h1>
@@ -115,6 +119,7 @@ function App() {
           <input type="checkbox"
             id="per-character"
             name="per-character"
+            checked={perCharacter ? true : false}
             onClick={handleSplitPerCharacterChange}
           />
           <label htmlFor="per-character">Split per character</label>
@@ -158,6 +163,44 @@ function App() {
             />
           </form>
         </div>
+      </div>
+      <h2>Quick fill</h2>
+      <div className="quick-fill-wrapper">
+        <button onClick={() => {
+          quickFill('1 2 3 4 5 6 7 8 9 10')
+          setPerCharacter(false)
+          setLineBreak(false)
+        }}>1 - 10</button>
+        <button onClick={() => {
+          quickFill('1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100')
+          setPerCharacter(false)
+          setLineBreak(false)
+        }}>1 - 100</button>
+        <button onClick={() => {
+          quickFill('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+          setPerCharacter(true)
+          setLineBreak(false)
+        }}>A - Z</button>
+        <button onClick={() => {
+          quickFill('abcdefghijklmnopqrstuvwxyz')
+          setPerCharacter(true)
+          setLineBreak(false)
+        }}>a - z</button>
+        <button onClick={() => {
+          quickFill('AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz')
+          setPerCharacter(true)
+          setLineBreak(false)
+        }}>Aa - Zz</button>
+        <button onClick={() => {
+          quickFill('Monday Tuesday Wednesday Thursday Friday Saturday Sunday')
+          setPerCharacter(false)
+          setLineBreak(false)
+        }}>Mon - Sun</button>
+        <button onClick={() => {
+          quickFill('January February March April May June July August September October November December')
+          setPerCharacter(false)
+          setLineBreak(false)
+        }}>Jan - Dec</button>
       </div>
     </div>
   )
